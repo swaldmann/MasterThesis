80373b901184382d2590e31e947d89e938326284
Steffen Waldmann <wa.steffen@me.com>
2020-09-16 06:13:22 +0200

    Updates

---
+++ precache-manifest.facf301c9546e4e6778789b376320f2f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6750d0b447c5fb72c7b38193c71c8c6d",
+    "url": "/dbs-visualization/index.html"
+  },
+  {
+    "revision": "f5a537fcb8fe06653465",
+    "url": "/dbs-visualization/static/css/2.ed590494.chunk.css"
+  },
+  {
+    "revision": "cadb277e433faaa16e45",
+    "url": "/dbs-visualization/static/css/main.22c87a32.chunk.css"
+  },
+  {
+    "revision": "f5a537fcb8fe06653465",
+    "url": "/dbs-visualization/static/js/2.1be9c672.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/dbs-visualization/static/js/2.1be9c672.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cadb277e433faaa16e45",
+    "url": "/dbs-visualization/static/js/main.5cb1ce5b.chunk.js"
+  },
+  {
+    "revision": "b9991685deddbe022ad4",
+    "url": "/dbs-visualization/static/js/runtime-main.9783475f.js"
+  }
+]);
