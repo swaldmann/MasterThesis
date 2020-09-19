29b05c7b4e6c1438ced9c292e812967a7c90756a
Steffen Waldmann <wa.steffen@me.com>
2020-09-19 02:22:16 +0200

    Add persistence

--- client/src/index.js
+++ client/src/index.js
@@ -7,20 +7,34 @@ import { Provider } from 'react-redux'
 import thunk from 'redux-thunk'
 import rootReducer from './reducers'
 import * as serviceWorker from './serviceWorker'
+import { persistStore, persistReducer } from 'redux-persist'
+import storage from 'redux-persist/lib/storage' // defaults to localStorage for web
+import { PersistGate } from 'redux-persist/integration/react'
+
+const persistConfig = {
+    key: 'root',
+    storage
+}
+
+const persistedReducer = persistReducer(persistConfig, rootReducer)
 
 export const store = createStore(
-    rootReducer,
-    //applyMiddleware(thunk)//,
+    persistedReducer,
+    applyMiddleware(thunk)//,
     // Change to this if you want to use the Redux Devtools extension in Chrome.
-    compose(
+    /*compose(
         applyMiddleware(thunk),
         window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
-    )
+    )*/
 )
+
+const persistor = persistStore(store)
 
 ReactDOM.render(
     <Provider store={store}>
-        <App />
+          <PersistGate loading={null} persistor={persistor}>
+            <App />
+        </PersistGate>
     </Provider>,
     document.getElementById('root')
 );
