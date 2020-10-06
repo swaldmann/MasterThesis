2438e89ac3df7b7808967e11108c86e54f29b664
Steffen Waldmann <wa.steffen@me.com>
2020-10-06 05:31:51 +0200

    Add client implementation text and remove unused files

--- client/src/App.js
+++ client/src/App.js
@@ -2,14 +2,10 @@ import React from 'react'
 import './styles/App.css'
 
 import AlgorithmVisualization from './AlgorithmVisualization'
-//import ART from './ART'
 
 function App() {
     return (
         <div className="App">
-            {/*<header className="App-header">
-                <h1>Database Algorithms Visualization</h1>
-            </header>*/}
             <AlgorithmVisualization />
         </div>
     );
